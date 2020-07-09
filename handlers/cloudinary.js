6dbb4896915048dc01d44653614aa68fc427af50
Deepam Bahre <56350721+deepam-bahre-au6@users.noreply.github.com>
2020-07-09 15:32:13 +0530

    Update cloudinary.js

--- handlers/cloudinary.js
+++ handlers/cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dts4wxk4i',
+  api_key: process.env.CLOUDINARY_API_KEY || '498335738472758',
+  api_secret: process.env.CLOUDINARY_API_SECRET || 'mFI5WNXaJLZAl9AK4imtW7Xc7uw',
 });

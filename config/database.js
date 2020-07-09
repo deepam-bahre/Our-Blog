064b61f090ffdd0b76e2a212c6dce552b2fc33c9
Deepam Bahre <56350721+deepam-bahre-au6@users.noreply.github.com>
2020-07-09 12:23:30 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@
 // =================================================
 
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
+ dotenv.config({ path: '.env' });
 /*
 --------------For Local--------------
 mongoose

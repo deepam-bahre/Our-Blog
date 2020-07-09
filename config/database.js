501df7cd79f62dc8e41b39c3649bb025f2b1b5d4
Deepam Bahre <56350721+deepam-bahre-au6@users.noreply.github.com>
2020-07-09 12:05:55 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -20,14 +20,12 @@ mongoose
 */
 
 /*--------------For Cloud--------------*/
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("database connected successfully");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+.then(() => {
+  console.log("database connected successfully");
+})
+.catch((err) => {
+  console.log(err.message);
+});
